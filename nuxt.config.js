03200802ca4eee1a0be92dd1176b266c4b08c657
samternent <sam.ternent@gmail.com>
2018-11-10 03:12:44 +0000

    remove google font

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,6 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'canonical', href: '/' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto' }
     ]
   },
 
