698492c9607ef2406a3539134d1e44dc5c5979ec
samternent <sam.ternent@gmail.com>
2018-11-29 16:16:43 +0000

    fix blur

--- cms/rehash-trash/contact.js
+++ cms/rehash-trash/contact.js
@@ -11,7 +11,7 @@ export default {
     {
       content: 'Rehash Trash (showroom & workshop)\n\nSombai Road (next door to Sombai Wine shop)\nSiem Reap, Cambodia 17000\n\nOpen daily, 08.00 – 17.00\n\nRehash Trash (market stall)\n\nMade In Cambodia Market\nKings Road Angkor\nAchar Sva Street / Street 27\nSiem Reap, Cambodia 17259\n\nOpen daily, 12.00 – 22.00',
       image: 'images/rehash-trash/contact.jpeg',
-      ctaLink: 'https://alink.com',
+      blur: '5px',
     },
   ],
 };
--- cms/rehash-trash/our-story.js
+++ cms/rehash-trash/our-story.js
@@ -5,24 +5,33 @@ export default {
   image: 'images/rehash-trash/2.jpeg',
   stories: [
     {
+      image: 'images/rehash-trash/2.jpeg',
+      blur: '5px',
+    },
+    {
       content: 'Rehash Trash is a female empowerment, skills-training and environmental clean-up initiative all in one. Through upcycling solutions to Cambodia’s plastic pollution, we employ marginalised Khmer women to create products of value, so they can support their families and transform their lives.',
       image: 'images/rehash-trash/ourstory-1.jpeg',
+      blur: '5px',
     },
     {
       content: 'Our products are 100% upcycled from discarded materials rescued from the environment. Rehash Trash is creating sustainable and fair-trade employment opportunities for our women, and ethical shopping with real social and eco impact for you.',
       image: 'images/rehash-trash/ourstory-2.jpeg',
+      blur: '5px',
     },
     {
       content: 'Rehash Trash is a social enterprise from the Green Gecko Project, a Cambodian NGO working with former street kids. It was developed to focus on the mothers of those children, creating long-term solutions for the families and enabling the kids to stay in full-time education.',
       image: 'images/rehash-trash/ourstory-3.jpeg',
+      blur: '5px',
     },
     {
       content: 'Our women were from uneducated and often abusive backgrounds; some also suffered from disabilities or addictions. They had never before had the opportunity of a job that was neither degrading or dangerous.',
       image: 'images/rehash-trash/ourstory-4.jpeg',
+      blur: '5px',
     },
     {
       content: 'Rehash Trash teaches them new skills and provides a decent income in a safe, happy and stable work environment. Rehash Trash: empowering women through sustainable employment while also cleaning up the environment by turning trash into treasure!',
       image: 'images/rehash-trash/ourstory-5.jpeg',
+      blur: '5px',
     },
   ],
 };
