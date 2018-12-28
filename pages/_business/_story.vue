ecb8aa96ce5463ca156f710f14f2dbc4ae94698e
samternent <sam.ternent@gmail.com>
2018-12-28 13:00:17 +0000

    removed unused transitions

--- pages/_business/_story.vue
+++ pages/_business/_story.vue
@@ -11,11 +11,6 @@
 import Story from '@/components/Story';
 export default {
   components: { Story },
-  // key: to => to.fullPath,
-  transition(to, from) {
-    if (!from) return 'slide-left'
-    return +to.query.page < +from.query.page ? 'slide-right' : 'slide-left'
-  },
   async asyncData({ app, params }) {
     const { business } = await import(`@/cms/${params.business}`);
     const story = await import(`@/cms/${params.business}/${params.story}.js`);
