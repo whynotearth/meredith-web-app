07b7b29cbeb0842e97e03184eb7f0b454a30ff2f
samternent <sam.ternent@gmail.com>
2018-12-28 15:22:32 +0000

    adds basic stripe test

--- pages/_business/checkout.vue
+++ pages/_business/checkout.vue
@@ -1,5 +1,49 @@
 <template>
-  <div>
-    checkout page
+  <div
+    class="container">
+    <section class="card__wrapper">
+      <div ref="card"/>
+    </section>
   </div>
 </template>
+
+<script>
+export default {
+  mounted: function () {
+    const stripe = Stripe(`YOUR_STRIPE_PUBLISHABLE_KEY`);
+    const elements = stripe.elements();
+
+    const card = elements.create('card');
+    card.mount(this.$refs.card);
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  position: relative;
+}
+
+
+.card__wrapper {
+  position: relative;
+  display: block;
+  height: 100vh;
+  overflow: hidden;
+  perspective: 640px;
+  perspective-origin: 0 50%;
+}
+@media (min-width: 550px){
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+  }
+  .card__wrapper {
+    width: 320px;
+    height: 570px;
+    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  }
+}
+</style>
