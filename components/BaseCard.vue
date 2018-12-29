5fe9ff8709d37aec29d0e8cbf88441801e457777
Nazreen Mohamad <nxmohamad@gmail.com>
2018-12-30 02:09:31 +0800

    create BaseCard

---
+++ components/BaseCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div
+      :style="content.style"
+      class="card__wrapper"
+    >
+      <header v-if="content.title">
+        {{ content.title }}
+      </header>
+      <main>
+        <slot />
+      </main>
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseCard",
+  props: {
+    content: {
+      type: Object,
+      default: () => {}
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.card__wrapper {
+  position: relative;
+  display: block;
+  height: 100vh;
+  overflow: hidden;
+  perspective: 640px;
+  perspective-origin: 0 50%;
+  padding: 50px 10px;
+  header {
+    text-align: center;
+    font-size: 1.8em;
+    padding: 10px
+  }
+}
+@media (min-width: 550px) {
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+  }
+  .card__wrapper {
+    width: 320px;
+    height: 570px;
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
+}
+</style>
+
