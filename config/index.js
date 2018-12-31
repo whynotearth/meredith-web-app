b8eb1f5eebf07186f9d671be28c0bc8fa9ff65b8
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-01 02:41:32 +0800

    set default donation amount

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 export default {
   TEST_STRIPE_KEY: "pk_test_Hl82FbuXgZzrCxpyY4h9ilJl",
-  API_URL: process.env.API_URL || "https://stagingapi.whynot.earth"
+  API_URL: process.env.API_URL || "https://stagingapi.whynot.earth/api/v0",
+  DEFAULT_DONATION_AMOUNT: 5.00,
 }
