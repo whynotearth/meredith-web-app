ec69a5cce5c5626210469c0fb7c1285f7ec61e99
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-03 03:30:20 +0800

    add test companyId

--- config/index.js
+++ config/index.js
@@ -2,4 +2,5 @@ export default {
   TEST_STRIPE_KEY: "pk_test_Hl82FbuXgZzrCxpyY4h9ilJl",
   API_URL: process.env.API_URL || "https://stagingapi.whynot.earth/api/v0",
   DEFAULT_DONATION_AMOUNT: 5.00,
+  TEST_COMPANY_ID: "8ed6810d-7a54-4f2a-add7-fe13fde8437b"
 }
