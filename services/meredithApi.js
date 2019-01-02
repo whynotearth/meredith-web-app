fd5eaa55a5bd89566d6919b85b44b20bb586ba8e
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-03 03:30:43 +0800

    include companyId

--- services/meredithApi.js
+++ services/meredithApi.js
@@ -1,16 +1,21 @@
 import config from '@/config'
 import axios from 'axios'
 
-async function postStripeTransaction ({ token }) {
+async function postStripeTransaction({
+  token,
+  // TODO: remove below 2 default values after hooked to API
+  amount = config.DEFAULT_DONATION_AMOUNT,
+  companyId = config.TEST_COMPANY_ID
+}) {
   try {
     const payload = {
       token,
-      amount: config.DEFAULT_DONATION_AMOUNT
-      //companyId
+      amount,
+      companyId
     }
-    const result = await axios.post(config.API_URL + '/stripe/charge/create', payload)
-  } catch(e) {
-    console.error(e)
+    await axios.post(config.API_URL + '/stripe/charge/create', payload)
+  } catch (e) {
+    throw e
   }
 }
 
