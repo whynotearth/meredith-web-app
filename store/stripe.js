60ad977c6db9faf201e16fd427054aa2c8509fcc
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-03 03:30:31 +0800

    create store

---
+++ store/stripe.js
@@ -0,0 +1,30 @@
+import { meredithApi } from '@/services'
+
+const transactionStatusOptions = {
+  SUBMITTING: 'submitting',
+  SUCCESSS: 'success',
+  ERROR: 'error'
+}
+
+export const state = () => ({
+  transactionStatus: null,
+  error: null
+})
+
+export const actions = {
+  async postStripeTransaction({ commit }, payload) {
+    try {
+      await meredithApi.postStripeTransaction(payload)
+      commit('updateTransactionStatus', transactionStatusOptions.SUCCESSS)
+    } catch (e) {
+      commit('updateTransactionStatus', transactionStatusOptions.ERROR, e)
+    }
+  }
+}
+
+export const mutations = {
+  updateTransactionStatus(state, status, error) {
+    state.transactionStatus = status
+    state.error = error
+  }
+}
