22e735082350562b58db324a92f45e7dc0f3b524
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-05 02:44:04 +0800

    temp toggle Loading component

--- pages/_business/checkout.vue
+++ pages/_business/checkout.vue
@@ -1,32 +1,49 @@
 <template>
   <base-card :attributes="checkoutCard">
-    <stripe-form />
+    <loading />
+    <!-- <loading v-if="transactionStatus === 'submitting'" /> -->
+    <!-- <stripe-form v-else/> -->
   </base-card>
 </template>
 
 <script>
-import StripeForm from '@/components/StripeForm'
-import BaseCard from '@/components/BaseCard'
+import StripeForm from "@/components/StripeForm";
+import BaseCard from "@/components/BaseCard";
+import Loading from "@/components/Loading";
+import { mapState, mapActions } from "vuex";
+
 export default {
+  name: "Checkout",
   components: {
     StripeForm,
-    BaseCard
+    BaseCard,
+    Loading
   },
-  data () {
+  data() {
     return {
       checkoutCard: {
-        name: 'Checkout',
-        title: 'Stripe checkout form',
+        name: "Checkout",
+        title: "Stripe checkout form",
         style: {
           backgroundColor: "#17304A",
           color: "#FBFBFB"
         }
       }
-    }
+    };
+  },
+  computed: {
+    ...mapState({
+      transactionStatus: state => state.stripe.transactionStatus
+    })
+  },
+  created() {
+    this.updateTransactionStatus(null);
+  },
+  methods: {
+    ...mapActions({ updateTransactionStatus: "stripe/updateTransactionStatus" })
   }
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
