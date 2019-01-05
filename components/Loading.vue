91019e84e4be26945ecc75c7c893865845614e8c
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-06 03:04:15 +0800

    tidy up scss

--- components/Loading.vue
+++ components/Loading.vue
@@ -7,34 +7,35 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
 .lds-ring {
   display: inline-block;
   position: relative;
   width: 64px;
   height: 64px;
-}
-.lds-ring div {
-  box-sizing: border-box;
-  display: block;
-  position: absolute;
-  width: 51px;
-  height: 51px;
-  margin: 6px;
-  border: 6px solid #fff;
-  border-radius: 50%;
-  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-  border-color: #fff transparent transparent transparent;
-}
-.lds-ring div:nth-child(1) {
+  div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 51px;
+    height: 51px;
+    margin: 6px;
+    border: 6px solid #fff;
+    border-radius: 50%;
+    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: #fff transparent transparent transparent;
+  }
+  div:nth-child(1) {
   animation-delay: -0.45s;
+  }
+  div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+  div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
 }
-.lds-ring div:nth-child(2) {
-  animation-delay: -0.3s;
-}
-.lds-ring div:nth-child(3) {
-  animation-delay: -0.15s;
-}
+
 @keyframes lds-ring {
   0% {
     transform: rotate(0deg);
@@ -43,4 +44,5 @@
     transform: rotate(360deg);
   }
 }
+
 </style>
