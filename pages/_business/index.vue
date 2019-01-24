83343412e7aa070c10d657edf980764e591e1e67
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-25 02:56:14 +0800

    use store

--- pages/_business/index.vue
+++ pages/_business/index.vue
@@ -1,5 +1,6 @@
 <template>
   <story
+    v-if="business"
     :brand="business.name"
     :home="business.id"
     :logo="business.logo"
@@ -8,14 +9,23 @@
 <script>
 import Story from '@/components/Story';
 
+import { mapState } from 'vuex'
+
 export default {
   components: { Story },
-  async asyncData({ store, params }) {
+  async fetch({ store, params }) {
     await store.dispatch('business/getBusiness', params.business)
-    const { stories, business } = await import(`@/cms/${params.business}`);
-    console.log(store.state.business)
-    return { story: { title: business.title, stories }, business };
+    // const { stories, business } = await import(`@/cms/${params.business}`);
+    // const { business, stories } = store.state.business
+    // return { story: { title: business.title, stories }, business };
   },
+  computed: {
+    ...mapState({
+      business: state => state.business.business,
+      story: state => ({ title: state.business.business.title, stories: state.business.stories }),
+      storeStatus: state => state.business.storeStatus
+    })
+  }
 }
 </script>
 
