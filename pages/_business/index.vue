f7860ccbf3df39faf5078ddde25773342d78d081
Nazreen <nxmohamad@gmail.com>
2019-01-25 03:00:00 +0800

    feat(business): setup vuex module (#21)
    * create getBusiness method
    
    * create module
    
    * include method
    
    * update asyncData
    
    * use store
    
    * return
    
    * fix name
    
    * add todo
    
    * add todo

--- pages/_business/index.vue
+++ pages/_business/index.vue
@@ -1,5 +1,6 @@
 <template>
   <story
+    v-if="business"
     :brand="business.name"
     :home="business.id"
     :logo="business.logo"
@@ -7,12 +8,21 @@
 </template>
 <script>
 import Story from '@/components/Story';
+
+import { mapState } from 'vuex'
+// TODO: display loading when storeStatus is loading
 export default {
   components: { Story },
-  async asyncData({ app, params }) {
-    const { stories, business } = await import(`@/cms/${params.business}`);
-    return { story: { title: business.title, stories }, business };
+  async fetch({ store, params }) {
+    await store.dispatch('business/getBusiness', params.business)
   },
+  computed: {
+    ...mapState({
+      business: state => state.business.business,
+      story: state => ({ title: state.business.business.title, stories: state.business.stories }),
+      storeStatus: state => state.business.storeStatus
+    })
+  }
 }
 </script>
 
