19653429844f430edfd005dab2e73b1a74527d3c
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-25 02:58:29 +0800

    add todo

--- pages/_business/index.vue
+++ pages/_business/index.vue
@@ -10,14 +10,11 @@
 import Story from '@/components/Story';
 
 import { mapState } from 'vuex'
-
+// TODO: display loading when storeStatus is loading
 export default {
   components: { Story },
   async fetch({ store, params }) {
     await store.dispatch('business/getBusiness', params.business)
-    // const { stories, business } = await import(`@/cms/${params.business}`);
-    // const { business, stories } = store.state.business
-    // return { story: { title: business.title, stories }, business };
   },
   computed: {
     ...mapState({
